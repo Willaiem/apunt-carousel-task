d6b78b4f44e92422c2751ceb621d5cb7be8c5e00
Will4_U <vvwill4u@gmail.com>
2022-08-17 02:57:33 +0200

    feat: imported Carousel and offers.json and used them

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-
+import { Carousel } from './Carousel'
+import { offers } from './json/offers.json'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-
+    <Carousel offers={offers} />
   </React.StrictMode>
 )
