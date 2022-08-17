ee3ff64598927582a0104c5ba111e0e950ba43df
Will4_U <vvwill4u@gmail.com>
2022-08-17 03:12:53 +0200

    feat: Card - set up the needed variables from offer parameter

--- src/Card.tsx
+++ src/Card.tsx
@@ -2,7 +2,9 @@ type CardProps = {
   offer: Offer
 }
 export const Card = ({ offer }: CardProps) => {
-  
+  const { images: [img], sellingMode, view: { url } } = offer
+  const { location: { label: city } } = offer.allegroLocally
+
   return (
     <article className={styles.card}>
       
